15f43aebd772f3dfcaec779d2323a1475e123045
Varun Sudhir <98942379+varun10sudhir@users.noreply.github.com>
2024-08-25 11:51:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors'); // Import the cors package
 
 // Middleware to parse JSON requests
 app.use(express.json());
